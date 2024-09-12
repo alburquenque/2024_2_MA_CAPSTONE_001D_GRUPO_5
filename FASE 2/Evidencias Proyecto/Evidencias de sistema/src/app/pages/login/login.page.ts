3e53885e1b94e028c9faa63f1959de809202da00
batmanhaitiano <da.alburquenque@duocuc.cl>
2024-09-12 19:25:07 -0300

    Avance frontend

---
+++ FASE 2/Evidencias Proyecto/Evidencias de sistema/src/app/pages/login/login.page.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage {
+  email: string = '';
+  password: string = '';
+
+  constructor(private router: Router, private alertCtrl: AlertController) {}
+
+}
