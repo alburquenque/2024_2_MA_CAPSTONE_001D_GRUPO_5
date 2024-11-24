469de0631c2f37399fe8321c64d142927898f12c
elKevgodoy <k.godoy@duocuc.cl>
2024-11-23 21:53:06 -0300

    Arreglo general de la aplicacion

--- FASE 2/Evidencias Proyecto/Evidencias de sistema/Back-end/src/app/app.component.ts
+++ FASE 2/Evidencias Proyecto/Evidencias de sistema/Back-end/src/app/app.component.ts
@@ -6,44 +6,67 @@ import { Router } from '@angular/router';
 import { Platform } from '@ionic/angular';
 import { App } from '@capacitor/app';
 import { AuthGuard } from './guards/auth.guard';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-
-
-  constructor(private authService: AuthService, private router: Router, private platform: Platform, private authguard : AuthGuard) {
+export class AppComponent implements OnInit {
+  constructor(
+    private authService: AuthService, 
+    private router: Router, 
+    private platform: Platform, 
+    private authguard: AuthGuard
+  ) {
     this.addAllIcons();
+    this.initializeApp(); // Llamamos a initializeApp en el constructor
   }
 
   async ngOnInit() {
     await this.authService.initializeSession();
-    console.log('Session a sido restaurada')
+    console.log('Session ha sido restaurada');
   }
 
   addAllIcons() {
     addIcons({
       closeOutline
     });
-    }
+  }
 
   initializeApp() {
     this.platform.ready().then(() => {
+      // Configuramos el listener para deep links
       App.addListener('appUrlOpen', (event: any) => {
         console.log('Deep link recibido:', event);
-        const slug = event.url.split('com.scanbuy.app://').pop();
-        if (slug) {
-          const urlParams = new URLSearchParams(slug.split('?')[1]);
-          const token = urlParams.get('token_ws');
+        
+        // Manejamos tanto el esquema personalizado como la URL de WebPay
+        if (event.url.includes('com.scanbuy.app://')) {
+          const slug = event.url.split('com.scanbuy.app://').pop();
+          if (slug) {
+            const urlParams = new URLSearchParams(slug.split('?')[1]);
+            const token = urlParams.get('token_ws');
+            
+            if (token) {
+              console.log('Token recibido:', token);
+              this.router.navigate(['/payment/confirmation'], {
+                queryParams: { token_ws: token }
+              });
+            }
+          }
+        } else if (event.url.includes('webpay-scanbuy.onrender.com')) {
+          // Manejamos la redirección desde WebPay
+          const url = new URL(event.url);
+          const token = url.searchParams.get('token_ws');
           
-          this.router.navigate(['/payment/confirmation'], {
-            queryParams: { token_ws: token }
-          });
+          if (token) {
+            console.log('Token recibido desde WebPay:', token);
+            this.router.navigate(['/payment/confirmation'], {
+              queryParams: { token_ws: token }
+            });
+          }
         }
       });
     });
   }
-
 }
